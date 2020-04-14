595e29f55220f63f3a104646526ab4fc01fddc4b
ilyatroshko <troshko.00@mail.ru>
2020-04-14 11:19:23 +0300

    two commit

--- src/rppba/AdminMenu.java
+++ src/rppba/AdminMenu.java
@@ -1,8 +1,30 @@
 package rppba;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class AdminMenu {
+
+    @FXML
+    private Button addManager;
+
     public void addManager() {
         addManager.getScene().getWindow().hide();
         LoadScene.INSTANCE.getInstance().sceneLoader("addmanager", "Add worker");
+    }
+
+    public void addSupervisor(){
+        addManager.getScene().getWindow().hide();
+        LoadScene.INSTANCE.getInstance().sceneLoader("addsupervisor", "Add supervisor");
+    }
+
+    public void logOut() {
+        addManager.getScene().getWindow().hide();
+        LoadScene.INSTANCE.getInstance().sceneLoader("login", "Log IN");
+    }
+
+    public void deleteManager(){
+        addManager.getScene().getWindow().hide();
+        LoadScene.INSTANCE.getInstance().sceneLoader("deletemanager", "Delete Manager");
     }
 }
